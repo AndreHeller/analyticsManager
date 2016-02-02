4ddd529066a30601e1285e134f320b0e5dd1146b
André Heller <anheller6@gmail.com>
2016-02-03 00:25:41 +0100

    Added Alerts - basic version

--- assets/src/reference.ts
+++ assets/src/reference.ts
@@ -2,6 +2,7 @@
 ///<reference path="./typings/gapi2.d.ts" />
 
 ///<reference path="./util/Promises/Promise.ts" />
+///<reference path="./util/StringM.ts" />
 
 ///<reference path="./app/Routes.ts" />
 ///<reference path="./app/Strings.ts" />
@@ -12,5 +13,7 @@
 ///<reference path="./app/services/LoginService.ts" />
 ///<reference path="./app/services/UIService.ts" />
 ///<reference path="./app/services/LoaderService.ts" />
+///<reference path="./app/services/AlertService.ts" />
 
 ///<reference path="./app/directives/loader/Loader.ts" />
+///<reference path="./app/directives/alert/Alert.ts" />
